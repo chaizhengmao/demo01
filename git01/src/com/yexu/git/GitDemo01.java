5517b059e152456431e88c0b71a48fb279d4ebdc
chaizhengmao <chaizhengmao@126.com>
2019-01-22 14:54:48 +0800

    zhangsan commit

--- git01/src/com/yexu/git/GitDemo01.java
+++ git01/src/com/yexu/git/GitDemo01.java
@@ -4,5 +4,6 @@ public class GitDemo01 {
 	public static void main(String[] args) {
 		System.out.println("GitDemo01.main()");
 		System.out.println("hello git111");
+		System.out.println("i am zhangsan");
 	}
 }
