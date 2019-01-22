c154ecd9d0f3c86ba5d3145fc067d8257bbc922d
chaizhengmao <chaizhengmao@126.com>
2019-01-22 14:38:48 +0800

    commit second

--- git01/src/com/yexu/git/GitDemo01.java
+++ git01/src/com/yexu/git/GitDemo01.java
@@ -3,6 +3,6 @@ package com.yexu.git;
 public class GitDemo01 {
 	public static void main(String[] args) {
 		System.out.println("GitDemo01.main()");
-		System.out.println("hello git");
+		System.out.println("hello git111");
 	}
 }
